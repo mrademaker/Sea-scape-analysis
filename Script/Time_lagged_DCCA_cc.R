f5d28be08e5175a9d89e76be14dfaf7aed4c3097
mrademaker <48674159+mrademaker@users.noreply.github.com>
2019-10-09 09:23:42 +0200

    updated_structure_seascapes

--- Script/Time_lagged_DCCA_cc.R
+++ Script/Time_lagged_DCCA_cc.R
@@ -6,12 +6,14 @@ library(rollRegres)
 ###############################################
 
 ## data_1
-x= c(-1.042061,-0.669056,-0.685977,-0.067925,0.808380,1.385235,1.455245,0.540762 ,0.139570,-1.038133,0.080121,-0.102159,-0.068675,0.515445,0.600459,0.655325,0.610604,0.482337,0.079108,-0.118951,-0.050178,0.007500,-0.200622)
+x=c(-1.042061,-0.669056,-0.685977,-0.067925,0.808380,1.385235,1.455245,0.540762 ,0.139570,-1.038133,0.080121,-0.102159,-0.068675,0.515445,0.600459,0.655325,0.610604,0.482337,0.079108,-0.118951,-0.050178,0.007500,-0.200622)
 ## data_2
-y= c(-2.368030,-2.607095,-1.277660,0.301499,1.346982,1.885968,1.765950,1.242890,-0.464786,0.186658,-0.036450,-0.396513,-0.157115,-0.012962,0.378752,-0.151658,0.774253,0.646541,0.311877,-0.694177,-0.412918,-0.338630,0.276635)
+y=c(-2.368030,-2.607095,-1.277660,0.301499,1.346982,1.885968,1.765950,1.242890,-0.464786,0.186658,-0.036450,-0.396513,-0.157115,-0.012962,0.378752,-0.151658,0.774253,0.646541,0.311877,-0.694177,-0.412918,-0.338630,0.276635)
 ## window size = 6
 k=5
 
+plot(draw_V1$Year,draw_V1$value)
+plot(draw_V2$Year,draw_V2$value)
 lagged_DCCA_CC=function(x,y,k,lags){
   
     ## calculate cumulated deviation time series Xt
@@ -25,10 +27,6 @@ lagged_DCCA_CC=function(x,y,k,lags){
     data$xx=xx
     data$yy=yy
     
-    par(mfrow=c(2,2))
-    plot(x=data$time,y=data$xx)
-    plot(x=data$time,y=data$yy)
-    
     store_f2_x=list()
     store_f2_y=list()
     store_f2_xy=list()
@@ -41,7 +39,7 @@ lagged_DCCA_CC=function(x,y,k,lags){
       for (i in 1:length(xx)){
         # so sliding window step size 1 untill i + window size reaches end of time series
         if (i + k + tau < length(xx)){
-            print(i)
+            #print(i)
             dat_x=data[i:(i+k),] #subset of x data in window
             dat_y=data[(i+tau):(i+k+tau),] #subset of y data in window
             
@@ -71,13 +69,15 @@ lagged_DCCA_CC=function(x,y,k,lags){
         #F_DCCA_xy=mean(store_f2_xy)
         ptauDCCA = F_DCCA_xy/(sqrt(F_DCCA_x)*sqrt(F_DCCA_y)) #equation 1.
         dcca_list=as.numeric(append(dcca_list,ptauDCCA))
-      }
-    ptauDCCA=as.data.frame(dcca_list)
-    ptauDCCA$lag=(0:(length(dcca_list)-1))
-    names(ptauDCCA)[names(ptauDCCA)=="dcca_list"]="corr"
+    }
+
+    ptauDCCA=matrix(dcca_list)#as.data.frame(dcca_list)
+    #ptauDCCA$lag=(0:(length(dcca_list)-1))
+    #names(ptauDCCA)[names(ptauDCCA)=="dcca_list"]="corr"
     return(ptauDCCA)
 }
 
 df=lagged_DCCA_CC(x=x,y=y,k=5,lags=c(0,1,2,3,4,5,6,7,8,9,10))
+df
 barplot(df$corr,xlab="lag",ylab="correlation")
-
+dcca_list
