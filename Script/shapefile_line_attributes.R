5823175cdb7f5a603bd6a44ad29e4ca963489da8
mrademaker <48674159+mrademaker@users.noreply.github.com>
2019-10-22 10:47:24 +0200

    dissim_trends

--- Script/shapefile_line_attributes.R
+++ Script/shapefile_line_attributes.R
@@ -6,17 +6,21 @@ path=("C:/Users/mrademaker/Documents/Research projects/STCNWS/DATRAS/cpue_length
 #file with species info----
 species_info=read.table(paste(path,"Data/spec_info.txt",sep=""),sep="\t",header=TRUE)
 species_info$file_name =gsub(" ","_",species_info$Scientific.name)
-
-spec=species_info$file_name[1]
-
-data=read.csv(paste(path,sprintf("Seascapes/Q1/%s/GAM/Diff_smooth/num_diff.csv",spec),sep=""))
-combis=c("1 - 2"  ,"1 - 3" ,"2 - 3" ,"2 - 4" ,"3 - 5" ,"3 - 6" ,"4 - 5" ,"5 - 10" ,"5 - 6"  ,"5 - 7" ,"5 - 8" ,"5 - 9" ,"6 - 8" ,"7 - 8" ,"7 - 9" ,"8 - 10" ,"8 - 9" ,"9 - 10")
-row510=data[35,]
-data <- data[data$combi %in% combis, ]
-data=data[-c(12),]
-data=InsertRow(data, NewRow = row510, RowNum = 8)
-spec
-min_ciw=min(data$ci_width)
-max_ciw=max(data$ci_width)
-range=max_ciw/min_ciw
-print(range)
+for (i in 1:nrow(species_info)){  
+  spec=species_info$file_name[i]
+  print(spec)
+  data=read.csv(paste(path,sprintf("Seascapes/Q1/%s/GAM/Diff_smooth/num_diff.csv",spec),sep=""))
+  combis=c("1 - 2"  ,"1 - 3" ,"2 - 3" ,"2 - 4" ,"3 - 5" ,"3 - 6" ,"4 - 5" ,"5 - 10" ,"5 - 6"  ,"5 - 7" ,"5 - 8" ,"5 - 9" ,"6 - 8" ,"7 - 8" ,"7 - 9" ,"8 - 10" ,"8 - 9" ,"9 - 10")
+  row510=data[35,]
+  data <- data[data$combi %in% combis, ]
+  data=data[-c(12),]
+  data=InsertRow(data, NewRow = row510, RowNum = 8)
+  spec
+  min_ciw=min(data$ci_width)
+  max_ciw=max(data$ci_width)
+  range=max_ciw/min_ciw
+  print(min_ciw)
+  print(max_ciw)
+  print(range)
+  print(max(data$period_diff))
+}
